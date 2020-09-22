e6e68a5f0769cb62a11c049971f7a1d2e6f1fc7b
Michael Thompson <mjt106@case.edu>
2020-09-22 13:56:53 -0400

    Adding more work into the client side command pattern

--- src/client_commander.py
+++ src/client_commander.py
@@ -7,8 +7,26 @@ Brief: This file navigates commands input from the client physically accessing t
 
 class ClientCommander:
     def __init__(self):
-        None
+        self._command_list = {}
+
+    def run(self):
+        """
+        Runs the core configuration and commands loop
+        """
+        command = ClientCommander.get_command()
+        self.parse_command(command)
+        while command is not 'x':
+            command = ClientCommander.get_command()
+            self.parse_command(command)
+
+        print("Shutting down")
 
     @staticmethod
-    def run():
-        print("Hello")
+    def get_command():
+        return input("Enter command (type list for all commands)\n")
+
+    def parse_command(self, command):
+        try:
+            self._command_list[command]
+        except KeyError:
+            print("Command \"" + command + "\" not found\n")
